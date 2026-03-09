c516750e4b2c68cb00a813d24a7c5932a95eee8c
deepanshusingla076 <deepanshusingla076@gmail.com>
2026-03-09 23:07:21 +0530

    add redis cloud

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,6 +1,6 @@
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379", {
+const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: 3,
   retryStrategy: (times) => Math.min(times * 200, 3000),
   enableReadyCheck: false,
